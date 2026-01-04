15e53179027795fbec658215f13e1b4cd555ac66
Steven Lakin <steven.m.lakin@gmail.com>
2026-01-04 14:59:18 -0600

    Working incremental DFS

--- src/sqlparser.py
+++ src/sqlparser.py
@@ -8,41 +8,39 @@ class DFS:
 	Construct tree using Depth First Search from nested intervals.
 	"""
 	def __init__(self, starts: list, stops: list) -> None:
-		self._seen = deque()  # DFS seen nodes
-		self._lhs = deque(starts)  # DFS unseen LHS
-		self._rhs = deque(stops)  # DFS unseen RHS
-		self.tree = {}  # Map: parent_node_idx -> [children_idx,]
+		self._level = 0  # Tree level
+		self._parent = deque()  # DFS seen open nodes LIFO
+		self._lhs = deque(starts)  # DFS unseen LHS FIFO
+		self._rhs = deque(stops)  # DFS unseen RHS FIFO
+		self.tree = {0: []}  # Map: parent_node_idx -> [children_idx,]
 		self.levels = {}  # Map: tree_level -> [node_idx,]
 		self.intervals = {}  # Map: node_idx -> [char_start_idx, char_stop_idx]
 		self.traversal = ()  # Tuple: (node_idx, ) traversal order
 		self._dfs(0)
 	
 	def _dfs(self, node):
-		if not self._rhs:
-			return
-		elif not self._lhs:
-			self._close_node(self._rhs.popleft())
-			self._dfs(self._seen.pop())  # up to parent
+		self.traversal += (node,)
+		if node not in self.tree:
+			self.levels.setdefault(self._level, []).append(node)
+			self.tree[self._parent[-1]].append(node)
+		self.tree.setdefault(node, [])
+		if not self._lhs:
+			self.intervals[node][1] = self._rhs.popleft()
+			self._level -= 1
 		elif self._rhs[0] < self._lhs[0]:
-			self._close_node(self._rhs.popleft())
-			self._dfs(self._seen.pop())  # up to parent
+			self.intervals[node][1] = self._rhs.popleft()
+			self._level -= 1
 		elif self._lhs[0] < self._rhs[0]:
-			self._open_node(node, self._lhs.popleft(), self._rhs.popleft())
+			self.intervals[len(self.tree)] = [self._lhs.popleft(), None]
+			self._parent.append(node)
+			self._level += 1
 			self._dfs(len(self.tree))  # down to child
 		else:
 			sys.stderr.write('ERROR: DFS.dfs() should not reach\n')
 			raise ValueError
-		
-	
-	def _open_node(self, node: int, start: int, stop: int) -> None:
-		x = 1
-
-	def _close_node(self, stop: int) -> None:
-		x = 1
-
-
-
-
+		if not self._rhs:
+			return
+		self._dfs(self._parent.pop())  # up to parent
 class SQLNode:
 	"""
 	One (sub-)query and its components.
@@ -67,10 +65,13 @@ class SQLTree:
 
 
 if __name__ == '__main__':
-	example1_nested = """
-		( ( ( ) ( ) ( ) ( ( ( ) ) ) ) ) ( ( ) ) ( ) ( ( ) )
-	"""
+	example1_nested = '1(3(5(7)9(12)15(18)21(24(27(30)33)36)39)42)45(48(51)54)57(60)63(66(69)72)'
 	opens = [match.start() for match in re.finditer(re.escape('('), example1_nested)]
 	closes = [match.start() for match in re.finditer(re.escape(')'), example1_nested)]
 	dfs = DFS(opens, closes)
+	print(example1_nested)
+	print('Traversal: {}'.format(dfs.traversal))
+	print('Tree: {}'.format(dfs.tree))
+	print('Levels: {}'.format(dfs.levels))
+	print('Intervals: {}'.format(dfs.intervals))
 	
