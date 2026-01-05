920039821fefceeded65f8646c5097ce5a8a27ca
Steven Lakin <steven.m.lakin@gmail.com>
2026-01-04 18:14:13 -0600

    DFS -> SQLTree

--- src/sqlparser.py
+++ src/sqlparser.py
@@ -16,6 +16,8 @@ class DFS:
 		self.levels = {}  # Map: tree_level -> [node_idx,]
 		self.intervals = {}  # Map: node_idx -> [char_start_idx, char_stop_idx]
 		self.traversal = ()  # Tuple: (node_idx, ) traversal order
+
+		assert(len(starts) == len(stops))
 		self._dfs(0)
 	
 	def _dfs(self, node):
@@ -41,6 +43,8 @@ class DFS:
 		if not self._rhs:
 			return
 		self._dfs(self._parent.pop())  # up to parent
+
+
 class SQLNode:
 	"""
 	One (sub-)query and its components.
@@ -59,9 +63,23 @@ class SQLTree:
 	"""
 	Stores query node types and relationships in tree structures.
 	"""
-	def __init__(self, full_query_text: str) -> None:
+	def __init__(self, full_query_text: str, ignore_strings = True) -> None:
+		self._ignore_idxs = set()
 		self.variables = {}
-		self.tree = {'root': SQLNode}
+		self.tree = {}
+		opens = [match.start() for match in re.finditer(re.escape('('), full_query_text)]
+		closes = [match.start() for match in re.finditer(re.escape(')'), full_query_text)]
+		if ignore_strings:
+			quote_idxs = [m.start() for m in re.finditer('[\'"]', full_query_text)]
+			assert(len(quote_idxs) % 2 == 0)
+			self._ignore_idxs = set([y for x in [list(range(quote_idxs[i], quote_idxs[i+1])) 
+												for i in range(0, len(quote_idxs), 2)] 
+									for y in x])
+			opens = [x for x in opens if x not in self._ignore_idxs]
+			closes = [x for x in closes if x not in self._ignore_idxs]
+			print(self._ignore_idxs)
+		self.dfs = DFS(opens, closes)
+
 
 
 if __name__ == '__main__':
@@ -73,5 +91,17 @@ if __name__ == '__main__':
 	print('Traversal: {}'.format(dfs.traversal))
 	print('Tree: {}'.format(dfs.tree))
 	print('Levels: {}'.format(dfs.levels))
-	print('Intervals: {}'.format(dfs.intervals))
+	print('Intervals: {}\n'.format(dfs.intervals))
+
+	example2_nested = '1(3(5(7)9(12)15(18)21(24(27(30)33)36)39)42)45(48(51)54)57(60)63(66(69)72)")"\')\''
+	sqltree = SQLTree(example2_nested)
+	print(example2_nested)
+	print('Traversal: {}'.format(sqltree.dfs.traversal))
+	print('Tree: {}'.format(sqltree.dfs.tree))
+	print('Levels: {}'.format(sqltree.dfs.levels))
+	print('Intervals: {}\n'.format(sqltree.dfs.intervals))
+	assert(dfs.traversal == sqltree.dfs.traversal)
+	assert(dfs.tree == sqltree.dfs.tree)
+	assert(dfs.levels == sqltree.dfs.levels)
+	assert(dfs.intervals == sqltree.dfs.intervals)
 	
